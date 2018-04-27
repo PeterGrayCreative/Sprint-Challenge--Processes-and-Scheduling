3f5cfa854ef5f9046890089c21821a9db5b12385
Peter Gray <peter@petergraycreative.com>
2018-04-27 12:21:54 -0400

    Commit working code to run commands, working on cd

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
 #define PROMPT "lambda-shell$ "
 
@@ -24,85 +24,98 @@
  *   args[3] ---> NULL (NULL is a pointer to address 0)
  *
  * @param str {char *} Pointer to the complete command line string.
- * @param args {char **} Pointer to an array of strings. This will hold the result.
- * @param args_count {int *} Pointer to an int that will hold the final args count.
+ * @param args {char **} Pointer to an array of strings. This will hold the
+ * result.
+ * @param args_count {int *} Pointer to an int that will hold the final args
+ * count.
  *
  * @returns A copy of args for convenience.
  */
-char **parse_commandline(char *str, char **args, int *args_count)
-{
-    char *token;
-    
-    *args_count = 0;
+char **parse_commandline(char *str, char **args, int *args_count) {
+  char *token;
 
-    token = strtok(str, " \t\n\r");
+  *args_count = 0;
 
-    while (token != NULL && *args_count < MAX_TOKENS - 1) {
-        args[(*args_count)++] = token;
+  token = strtok(str, " \t\n\r");
 
-        token = strtok(NULL, " \t\n\r");
-    }
+  while (token != NULL && *args_count < MAX_TOKENS - 1) {
+    args[(*args_count)++] = token;
 
-    args[*args_count] = NULL;
+    token = strtok(NULL, " \t\n\r");
+  }
 
-    return args;
+  args[*args_count] = NULL;
+
+  return args;
 }
 
 /**
  * Main
  */
-int main(void)
-{
-    // Holds the command line the user types in
-    char commandline[COMMANDLINE_BUFSIZE];
+int main(void) {
+  // Holds the command line the user types in
+  char commandline[COMMANDLINE_BUFSIZE];
 
-    // Holds the parsed version of the command line
-    char *args[MAX_TOKENS];
+  // Holds the parsed version of the command line
+  char *args[MAX_TOKENS];
 
-    // How many command line args the user typed
-    int args_count;
+  // How many command line args the user typed
+  int args_count;
 
-    // Shell loops forever (until we tell it to exit)
-    while (1) {
-        // Print a prompt
-        printf("%s", PROMPT);
-        fflush(stdout); // Force the line above to print
+  // Shell loops forever (until we tell it to exit)
+  while (1) {
+    // Print a prompt
+    printf("%s", PROMPT);
+    fflush(stdout);  // Force the line above to print
 
-        // Read input from keyboard
-        fgets(commandline, sizeof commandline, stdin);
+    // Read input from keyboard
+    fgets(commandline, sizeof commandline, stdin);
 
-        // Exit the shell on End-Of-File (CRTL-D)
-        if (feof(stdin)) {
-            break;
-        }
-
-        // Parse input into individual arguments
-        parse_commandline(commandline, args, &args_count);
-
-        if (args_count == 0) {
-            // If the user entered no commands, do nothing
-            continue;
-        }
-
-        // Exit the shell if args[0] is the built-in "exit" command
-        if (strcmp(args[0], "exit") == 0) {
-            break;
-        }
-
-        #if DEBUG
-
-        // Some debugging output
-
-        // Print out the parsed command line in args[]
-        for (int i = 0; args[i] != NULL; i++) {
-            printf("%d: '%s'\n", i, args[i]);
-        }
-
-        #endif
-        
-        /* Add your code for implementing the shell's logic here */
-        
+    // Exit the shell on End-Of-File (CRTL-D)
+    if (feof(stdin)) {
+      break;
     }
 
-    return 0;
+    // Parse input into individual arguments
+    parse_commandline(commandline, args, &args_count);
+
+    if (args_count == 0) {
+      // If the user entered no commands, do nothing
+      continue;
+    }
+
+    // Exit the shell if args[0] is the built-in "exit" command
+    if (strcmp(args[0], "exit") == 0) {
+      break;
+    }
+
+#if DEBUG
+
+    // Some debugging output
+
+    // Print out the parsed command line in args[]
+    for (int i = 0; args[i] != NULL; i++) {
+      printf("%d: '%s'\n", i, args[i]);
+    }
+
+#endif
+
+    /* Add your code for implementing the shell's logic here */
+    if (args[0] == 'cd') {
+      continue;
+    }
+    else if (args[0] == 'exit') {
+      break;
+    } 
+    else {
+      int child;
+      if (child = fork() == 0) {
+        execvp(args[0], args);
+      } else {
+        int wc = waitpid(child, NULL, 0);
+      }
+    }
+  }
+
+  return 0;
 }
